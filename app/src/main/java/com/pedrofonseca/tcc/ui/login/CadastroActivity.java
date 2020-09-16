dbaf1eca50860652ddccca66bbd9173690e16b01
Pedro Fonseca <reisensama2345@gmail.com>
2020-09-15 23:40:17 -0300

    Cadastro completo
    I guess?

--- app/src/main/java/com/pedrofonseca/tcc/ui/login/CadastroActivity.java
+++ app/src/main/java/com/pedrofonseca/tcc/ui/login/CadastroActivity.java
@@ -28,6 +28,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.pedrofonseca.tcc.R;
 import com.pedrofonseca.tcc.data.FirebaseAuthentication;
 import com.pedrofonseca.tcc.data.User;
@@ -170,7 +173,20 @@ public class CadastroActivity extends AppCompatActivity {
                     Toast.makeText(CadastroActivity.this, "Sucesso", Toast.LENGTH_SHORT).show();
                     finish();
             }else{
-                    Toast.makeText(CadastroActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+                    String exception= "";
+                    try{
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        exception = "Senha Fraca";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        exception = "E-Mail Invalido";
+                    } catch (FirebaseAuthUserCollisionException e) {
+                        exception = "Usuario Existente";
+                    } catch (Exception e) {
+                        exception = "Erro: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(CadastroActivity.this, exception, Toast.LENGTH_SHORT).show();
                 }
         }});
     }
